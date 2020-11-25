a88afa433ca4459642f0a9c12a5b0f4c2ba3103d
Mateus De Assis <aln13084.1@gmail.com>
2020-11-25 16:51:14 -0300

    trasformando tudo em func

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 // index === m === linha 
 // index = n cololuna 
 /* array vai receber uma coluna  */
-
 function cratArray(array, m) {
     for (let i = 0; i < m; i++) {
         //linha i vai rereber uma coluna [num de elementos === num colunas ]
@@ -27,17 +26,30 @@ function matris_principal(array) {
     for (let m = 0; m < array.length; m++) {
         for (let n = 0; n < array[m].length; n++) {
 
-            if (m != n) { array[m][n] = ''; }
+            if (m != n) { array[m][n] = '-'; }
         }
     }
     return array;
+}
+
+function alimenta_array(array) {
+
+    //pecorrendo o array e alimentando ele 
+    // m array
+    for (let i = 0; i < array.length; i++) {
+        //coluna array
+        for (let j = 0; j < 10; j++) {
+            //pisição aij :  i === linha, j === coluna
+            array[i][j] = Math.floor(10 * Math.random());
+        }
+    }
 }
 
 ///--MAIN--///////////--MAIN--////////--MAIN--//////
 
 
 //criando a matriz
-let arrayBi_D = [];
+let arrayBiD = [];
 
 //Numero de linhas que a matris vai ter 
 let num_m = 7;
@@ -45,20 +57,12 @@ let num_m = 7;
 
 //chamada da função que determina o numero de linhas == m; 
 //parametros o array e o numero de linhas
-cratArray(arrayBi_D, num_m);
+cratArray(arrayBiD, num_m);
 
-//pecorrendo o array e alimentando ele 
 
-// m array
-for (let i = 0; i < arrayBi_D.length; i++) {
 
-    //coluna array
-    for (let j = 0; j < 10; j++) {
 
-        //pisição aij :  i === linha, j === coluna
-        arrayBi_D[i][j] = Math.floor(10 * Math.random());
-    }
-}
 
-console.table(arrayBi_D)
-console.table(matris_principal(arrayBi_D))
+alimenta_array(arrayBiD)
+console.table(arrayBiD)
+console.table(matris_principal(arrayBiD))
