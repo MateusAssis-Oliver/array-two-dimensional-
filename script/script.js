414f4738e3c04fef6c720e68df779607889716d4
Mateus De Assis <aln13084.1@gmail.com>
2020-11-25 16:10:30 -0300

    add func mat_princ

--- script/script.js
+++ script/script.js
@@ -1,55 +1,36 @@
-// index === m linha 
+// index === m === linha 
 // index = n cololuna 
 /* array vai receber uma coluna  */
 
 function cratArray(array, m) {
-
     for (let i = 0; i < m; i++) {
-
         //linha i vai rereber uma coluna [num de elementos === num colunas ]
         array[i] = [];
     }
-
 }
 
 function zera_M(array) {
 
-
-
-
-    for (let i = 0; i < arrayBi_D.length; i++) {
+    for (let i = 0; i < array.length; i++) {
 
         //coluna array
-        for (let j = 0; j < arrai[i].length; j++) {
-
-            //pisição aij :  i === linha, j === coluna
-            arrayBi_D[i][j] = 0;
+        for (let j = 0; j < array[i].length; j++) {
+            array[i][j] = 0;
         }
     }
-
-
-
 }
 
-
-function exib_M(array) {
-
-    console.log(array.length)
-
-}
-
+function exib_M(array) { console.log(array.length) }
 
 function matris_principal(array) {
 
+    for (let m = 0; m < array.length; m++) {
+        for (let n = 0; n < array[m].length; n++) {
 
-    for (let m = 0; m < arrai.length; i++) {
-
-        for (let n = 0; n < arrai[i].length; n++) {
-
-
+            if (m != n) { array[m][n] = ''; }
         }
     }
-
+    return array;
 }
 
 ///--MAIN--///////////--MAIN--////////--MAIN--//////
@@ -62,15 +43,13 @@ let arrayBi_D = [];
 let num_m = 7;
 
 
-
-
 //chamada da função que determina o numero de linhas == m; 
 //parametros o array e o numero de linhas
 cratArray(arrayBi_D, num_m);
 
 //pecorrendo o array e alimentando ele 
 
-//linha array
+// m array
 for (let i = 0; i < arrayBi_D.length; i++) {
 
     //coluna array
@@ -82,8 +61,4 @@ for (let i = 0; i < arrayBi_D.length; i++) {
 }
 
 console.table(arrayBi_D)
-
-//num de colunas em um array
-let num_n = arrayBi_D[0].length;
-
-console.log(num_n)
+console.table(matris_principal(arrayBi_D))
